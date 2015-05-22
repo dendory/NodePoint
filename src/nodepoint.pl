405b36f68f4d9ba8b4603d32d5f04f1e4130933d
Patrick Lambert <dendory@gmail.com>
2015-05-22 09:58:04 -0400

    Added date field

--- src/nodepoint.pl
+++ src/nodepoint.pl
@@ -8,7 +8,7 @@
 #
 
 use strict;
-use Config::Linux;
+use Config::Win32;
 use Digest::SHA qw(sha1_hex);
 use DBI;
 use CGI;
@@ -27,7 +27,7 @@ my ($cfg, $db, $sql, $cn, $cp, $cgs, $last_login, $perf);
 my $logged_user = "";
 my $logged_lvl = -1;
 my $q = new CGI;
-my $VERSION = "1.2.1";
+my $VERSION = "1.2.2";
 my %items = ("Product", "Product", "Release", "Release", "Model", "SKU/Model");
 my @itemtypes = ("None");
 
@@ -54,6 +54,7 @@ sub headers
 	print "  <meta http-equiv='X-UA-Compatible' content='IE=edge'>\n";
 	print "  <meta name='viewport' content='width=device-width, initial-scale=1'>\n";
 	print "  <link rel='stylesheet' href='bootstrap.css'>\n";
+	print "  <link rel='stylesheet' href='datepicker.css'>\n";
 	if($cfg->load("css_template")) { print "  <link rel='stylesheet' href='" . $cfg->load("css_template") . "'>\n"; }
 	if($cfg->load("favicon")) { print "  <link rel='shortcut icon' href='" . $cfg->load("favicon") . "'>\n"; }
 	else { print "  <link rel='shortcut icon' href='favicon.gif'>\n"; }
@@ -76,6 +77,8 @@ sub footers
 	print " <script src='jquery.js'></script>\n";
 	print " <script src='bootstrap.js'></script>\n";
 	print " <script src='validator.js'></script>\n";
+	print " <script src='datepicker.js'></script>\n";
+	print " <script>\$('.datepicker').datepicker();</script>";
 	print " </body>\n";
 	print "</html>\n";
 }
@@ -723,11 +726,11 @@ sub home
 # Connect to config
 eval
 {
-	$cfg = Config::Linux->new("NodePoint", "settings");
+	$cfg = Config::Win32->new("NodePoint", "settings");
 };
 if(!defined($cfg)) # Can't even use headers() if this fails.
 {
-	print "Content-type: text/html\n\nError: Could not access " . Config::Linux->type . ". Please ensure NodePoint has the proper permissions.";
+	print "Content-type: text/html\n\nError: Could not access " . Config::Win32->type . ". Please ensure NodePoint has the proper permissions.";
 	exit(0);
 };
 
@@ -2716,6 +2719,7 @@ elsif($q->param('m')) # Modules
 						elsif(to_int($customform[($i*2)+3]) == 6) { print "<input type='email' class='form-control' name='field" . $i . "'>"; }
 						elsif(to_int($customform[($i*2)+3]) == 7) { print "<select class='form-control' name='field" . $i . "'><option>1</option><option>2</option><option>3</option><option>4</option><option>5</option><option>6</option><option>7</option><option>8</option><option>9</option><option>10</option></select>"; }
 						elsif(to_int($customform[($i*2)+3]) == 8) { print "<input type='text' class='form-control' name='field" . $i . "' value='" . $ENV{REMOTE_ADDR} . "' readonly>"; }
+						elsif(to_int($customform[($i*2)+3]) == 10) { print "<input type='text' class='form-control datepicker' name='field" . $i . "' placeholder='mm/dd/yyyy'>"; }
 						elsif(to_int($customform[($i*2)+3]) == 9) { print "<select class='form-control' name='field" . $i . "'><option>Extremely</option><option>A lot</option><option>Moderately</option><option>Slightly</option><option>Not at all</option></select>"; }
 						else { print "<input type='text' class='form-control' name='field" . $i . "'>"; }
 						print "</div></div></p>";
@@ -3198,7 +3202,9 @@ elsif(($q->param('create_form') || $q->param('edit_form') || $q->param('save_for
 			if(to_int($q->param('edit_form')) > 0) { if(to_int($res[($i*2)+3]) == 8) { print " selected"; } }
 			print ">IP address</option><option value=9";
 			if(to_int($q->param('edit_form')) > 0) { if(to_int($res[($i*2)+3]) == 9) { print " selected"; } }
-			print ">Satisfaction scale</option></td></tr>";
+			print ">Satisfaction scale</option><option value=10";
+			if(to_int($q->param('edit_form')) > 0) { if(to_int($res[($i*2)+3]) == 10) { print " selected"; } }
+			print ">Date</option></td></tr>";
 		}
 		print "</table><p><input type='submit' class='btn btn-primary pull-right' value='Save'></p></form></div></div>";
 	}
