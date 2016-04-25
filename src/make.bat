f5adddab307f7493c0d2005a8d8bf5e365bfc9a8
Patrick Lambert <dendory@gmail.com>
2016-04-25 09:21:32 -0400

    1.6.5

--- src/make.bat
+++ src/make.bat
@@ -1,4 +1,4 @@
 @call perlapp src\nodepoint.perlapp
 @call perlapp src\nodepoint-automate.perlapp
-#@call "C:\Program Files (x86)\Windows Kits\8.0\bin\x64\signtool.exe" sign /f c:\scripts\csc.pfx /t http://timestamp.verisign.com/scripts/timstamp.dll Windows\nodepoint\www\nodepoint.exe
-#@call "C:\Program Files (x86)\Windows Kits\8.0\bin\x64\signtool.exe" sign /f c:\scripts\csc.pfx /t http://timestamp.verisign.com/scripts/timstamp.dll Windows\nodepoint\www\nodepoint-automate.exe
+@call "C:\Program Files (x86)\Windows Kits\8.0\bin\x64\signtool.exe" sign /n "Patrick Lambert" /t http://timestamp.verisign.com/scripts/timstamp.dll Windows\nodepoint\www\nodepoint.exe
+@call "C:\Program Files (x86)\Windows Kits\8.0\bin\x64\signtool.exe" sign /n "Patrick Lambert" /t http://timestamp.verisign.com/scripts/timstamp.dll Windows\nodepoint\www\nodepoint-automate.exe
